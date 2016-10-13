251f070395fa5238d87c58a7f191de94eb613a3b
Nathaniel Vincent Romeo Charlebois <nathaniel.charlebois@gmail.com>
2016-10-13 17:38:13 -0400

    Cleaning up code

--- iteration_1/src/ServerThread.java
+++ iteration_1/src/ServerThread.java
@@ -1,5 +1,8 @@
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.Arrays;
 
 public abstract class ServerThread extends Thread{
 	
@@ -20,5 +23,92 @@ public abstract class ServerThread extends Thread{
 		System.out.println("Server: Exiting Gracefully");
 		System.exit(0);
 	}
+	
+    protected void printError(DatagramPacket packet){
+    	System.out.println("Server: Error packet received");
+    	System.out.println("From client: " + packet.getAddress());
+    	System.out.println("From client port: " + packet.getPort());
+	    System.out.println("Length: " + packet.getLength());
+	    System.out.println("Error Code: " + new String(packet.getData(),
+				   2,2));
+	    System.out.println("ErrorMessage: " );
+	    System.out.println(new String(packet.getData(),
+				   4,packet.getData().length-1));
+    }
+    
+    
+    //Build an Error Packet with format :
+    /*
+    2 bytes  2 bytes        string    1 byte
+    ----------------------------------------
+ERROR | 05    |  ErrorCode |   ErrMsg   |   0  |
+    ----------------------------------------
+    */
+    protected void buildError(int errorCode,DatagramPacket receivePacket, boolean verbose){
+    	int errorSizeFactor = 5;
+    	
+    	String errorMsg = new String("Unknown Error.");
+    	switch(errorCode){
+	    	case 1: errorCode = 1;
+	    		errorMsg = "File not found.";
+	    	case 2: errorCode = 2;
+	    		errorMsg = "Access violation.";
+	    	case 3: errorCode = 3;
+	    		errorMsg = "Disk full or allocation exceeded.";
+	    	case 4: errorCode = 6;
+	    		errorMsg = "File already exists.";
+    	}
+    	
+    	byte[] data = new byte[errorMsg.length() + errorSizeFactor];
+    	data[0] = 0;
+    	data[1] = 5;
+    	data[2] = 0;
+    	data[3] = (byte)errorCode;
+    	for(int c = 0; c<errorMsg.length();c++){
+    		data[4+c] = errorMsg.getBytes()[c];
+    	}
+    	data[data.length-1] = 0;
+    	
+	    DatagramPacket sendPacket = new DatagramPacket(data, data.length,
+				     receivePacket.getAddress(), receivePacket.getPort());
+		/* Exit Gracefully if the stop is requested. */
+		   if(stopRequested){exitGraceFully();}
+	       		System.out.println("Server: Sending packet:");
+	       if(verbose){
+		       System.out.println("To host: " + sendPacket.getAddress());
+		       System.out.println("Destination host port: " + sendPacket.getPort());
+		       
+		       System.out.println("Length: " + sendPacket.getLength());
+		       System.out.println("Containing: " );
+		       System.out.println(Arrays.toString(sendPacket.getData()));
+	       }
+
+
+	       	// Send the datagram packet to the client via a new socket.
+
+	       	try {
+	       		// Construct a new datagram socket and bind it to any port
+	       		// on the local host machine. This socket will be used to
+				// send UDP Datagram packets.
+		       	sendReceiveSocket = new DatagramSocket();
+	       	} catch (SocketException se) {
+	       		se.printStackTrace();
+	       		System.exit(1);
+	       	}
+
+	       	try {
+	       		sendReceiveSocket.send(sendPacket);
+	       	} catch (IOException e) {
+	       		e.printStackTrace();
+	       		System.exit(1);
+	       	}
+	       	/* Exit Gracefully if the stop is requested. */
+	       	if(stopRequested){exitGraceFully();}
+	       	if(verbose){
+	       		System.out.println("Server: packet sent using port " + sendReceiveSocket.getLocalPort());
+	    	  	System.out.println();
+	       	}
+    	
+    }
 
 }
--- iteration_1/src/TFTPReadThread.java
+++ iteration_1/src/TFTPReadThread.java
@@ -123,11 +123,11 @@ class TFTPReadThread  extends ServerThread implements Runnable
 				reader.readAndSplit(filename.toString());
 	
 			} catch (FileNotFoundException e1) {
-				buildError(1,receivePacket);
+				buildError(1,receivePacket,verbose);
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			} catch (IOException e1) {
-				buildError(3,receivePacket);
+				buildError(3,receivePacket,verbose);
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
@@ -236,86 +236,4 @@ class TFTPReadThread  extends ServerThread implements Runnable
 
 		//transcript.append(Thread.currentThread() + " finished\n");
 	}
-	private void buildError(int errorCode,DatagramPacket receivePacket){
-    	int errorSizeFactor = 5;
-    	
-    	String errorMsg = new String("Unknown Error.");
-    	switch(errorCode){
-    	case 1: errorCode = 1;
-    		errorMsg = "File not found.";
-    	case 2: errorCode = 2;
-    		errorMsg = "Access violation.";
-    	case 3: errorCode = 3;
-    		errorMsg = "Disk full or allocation exceeded.";
-    	case 4: errorCode = 6;
-    		errorMsg = "File already exists.";
-    	}
-    	
-    	byte[] data = new byte[errorMsg.length() + errorSizeFactor];
-    	data[0] = 0;
-    	data[1] = 5;
-    	data[2] = 0;
-    	data[3] = (byte)errorCode;
-    	for(int c = 0; c<errorMsg.length();c++){
-    		data[4+c] = errorMsg.getBytes()[c];
-    	}
-    	data[data.length-1] = 0;
-    	
-	    sendPacket = new DatagramPacket(data, data.length,
-				     receivePacket.getAddress(), receivePacket.getPort());
-		/* Exit Gracefully if the stop is requested. */
-		   if(stopRequested){exitGraceFully();}
-	       		System.out.println("Server: Sending packet:");
-	       if(verbose){
-		       System.out.println("To host: " + sendPacket.getAddress());
-		       System.out.println("Destination host port: " + sendPacket.getPort());
-		       
-		       System.out.println("Length: " + sendPacket.getLength());
-		       System.out.println("Containing: " );
-		       System.out.println(Arrays.toString(sendPacket.getData()));
-	       }
-
-
-	       	// Send the datagram packet to the client via a new socket.
-
-	       	try {
-	       		// Construct a new datagram socket and bind it to any port
-	       		// on the local host machine. This socket will be used to
-				// send UDP Datagram packets.
-		       	sendReceiveSocket = new DatagramSocket();
-	       	} catch (SocketException se) {
-	       		se.printStackTrace();
-	       		System.exit(1);
-	       	}
-
-	       	try {
-	       		sendReceiveSocket.send(sendPacket);
-	       	} catch (IOException e) {
-	       		e.printStackTrace();
-	       		System.exit(1);
-	       	}
-	       	/* Exit Gracefully if the stop is requested. */
-	       	if(stopRequested){exitGraceFully();}
-	       	if(verbose){
-	       		System.out.println("Server: packet sent using port " + sendReceiveSocket.getLocalPort());
-	    	  	System.out.println();
-	       	}
-    	
-    }
-	       
-
-
-    
-    private void printError(DatagramPacket packet){
-    	System.out.println("Server: Error packet received");
-    	System.out.println("From client: " + packet.getAddress());
-    	System.out.println("From client port: " + packet.getPort());
-	    System.out.println("Length: " + packet.getLength());
-	    System.out.println("Error Code: " + new String(packet.getData(),
-				   2,2));
-	    System.out.println("ErrorMessage: " );
-	    System.out.println(new String(packet.getData(),
-				   4,packet.getData().length-1));
-    }
-
 }
