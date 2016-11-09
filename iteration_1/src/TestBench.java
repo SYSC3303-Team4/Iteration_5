635cb594b3427f200df1c145809f3a52c4c4a603
Jason Van Kerkhoven <jason.vk@outlook.com>
2016-11-09 15:56:00 -0500

    launches 3 threads with 1 click

---
+++ iteration_1/src/TestBench.java
@@ -0,0 +1,66 @@
+/**
+*Class:             Console.java
+*Project:           TFTP Project - Group 4
+*Author:            Jason Van Kerkhoven                                             
+*Date of Update:    08/11/2016                                              
+*Version:           1.1.1                                                      
+*                                                                                   
+*Purpose:           Instead of launching 3 things, we launch 1 thing which launches 3 other things.
+* 
+* 
+*Update Log:		v1.0.0
+*						- I got sick of launching 3 things
+*						- instantiated client, server, host in main on seperate threads
+*/
+
+
+
+
+public class TestBench 
+{
+
+	public static void main(String[] args) 
+	{
+		//prep server thread
+		Thread threadA = new Thread()
+				{
+					public void run()
+					{
+						//this throws an exception I guess?????
+						try
+						{
+							TFTPServer.main(null);
+						}
+						catch (Exception e)
+						{
+							//do absolutely nothing
+						}
+					}
+				};
+				
+		//prep client thread
+		Thread threadB = new Thread()
+				{
+					public void run()
+					{
+						TFTPClient.main(null);
+					}
+				};
+		
+		//prep host thread
+		Thread threadC = new Thread()
+				{
+					public void run()
+					{
+						TFTPHost.main(null);
+					}
+				};
+		
+		//run all threads
+		threadA.start();
+		threadB.start();
+		threadC.start();
+		
+	}
+
+}
